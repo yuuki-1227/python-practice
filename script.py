09fbe28b441dbd7cc62fa3f3bf3320c07f20eacc
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-26 18:46:21 +0900

    [Update]

--- script.py
+++ script.py
@@ -1,2 +1,17 @@
 print('Hello, world!')
 print(1 + 2)
+
+import openpyxl
+  wb = openpyxl.load_workbook("Book1.xlsx")
+
+from pathlib import Path
+import openpyxl
+p = Path("./Book1.xlsx")
+if p.exists():
+  wb = openpyxl.load_workbook(p)
+
+import openpyxl
+  wb = openpyxl.load_workbook("Book1.xlsm", keep_vba=True)
+
+import openpyxl
+wb = openpyxl.Workbook()
