c5755e9cfc0fb9b992e78f05514a2d890b6c6fc9
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-27 17:42:44 +0900

    [Update]顔認証アプリ 画像アップロード

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+from PIL import Image
 
-st.title('My 1st App!!!')
+st.title('顔認識アプリ')
 
-st.write('データフレーム')
-# pd.DataFrame() Pandasのデータフレーム型を書くことができる
-st.write(
-    pd.DataFrame({
-        '1st column': [1, 2, 3, 4],
-        '2nd column': [10, 20, 30, 40]
-    })
-)
+# 画像投稿フォーム file_uploader()
+uploaded_file = st.file_uploader("Choose an image...", type='jpg')
+# if 変数 is not None: 変数の中に値が入っていたら、、、
+if uploaded_file is not None:
+    img = Image.open(uploaded_file)
+    st.image(img, caption='Uploaded Image.', use_column_width=True)
+ 
 
-"""
-# My 1st App
-こんな感じでマジックコマンドを使用できる。Markdown対応。
+# st.write('データフレーム')
+# # pd.DataFrame() Pandasのデータフレーム型を書くことができる
+# st.write(
+#     pd.DataFrame({
+#         '1st column': [1, 2, 3, 4],
+#         '2nd column': [10, 20, 30, 40]
+#     })
+# )
 
-"""
-# if文　checkboxにおけるtrue.falseで判定
-# ネストに注意
-if st.checkbox('Show DataFrame'):
-    # チャート作成
-    # np.random.randn 乱数作成、正規分布(平均0,標準偏差1)
-    chart_df = pd.DataFrame(
-        np.random.randn(20, 3),
-        columns=['a', 'b', 'c']
-    )
-    # チャートを表示
-    st.line_chart(chart_df)
+# """
+# # My 1st App
+# こんな感じでマジックコマンドを使用できる。Markdown対応。
+
+# """
+# # if文　checkboxにおけるtrue.falseで判定
+# # ネストに注意
+# if st.checkbox('Show DataFrame'):
+#     # チャート作成
+#     # np.random.randn 乱数作成、正規分布(平均0,標準偏差1)
+#     chart_df = pd.DataFrame(
+#         np.random.randn(20, 3),
+#         columns=['a', 'b', 'c']
+#     )
+#     # チャートを表示
+#     st.line_chart(chart_df)
