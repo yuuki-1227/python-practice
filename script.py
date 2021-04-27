86284e767ea87df0107770fc15cc3800de95d0bf
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-27 09:53:19 +0900

    [関数]

--- script.py
+++ script.py
@@ -7,15 +7,27 @@
 # print(1*2)
 
 # 変数
-test = 'l_size'
-tall = 5
-weight = 5.5
+# test = 'l_size'
+# tall = 5
+# weight = 5.5
 
-# print(login_id + test)
+# # print(login_id + test)
 
-# 条件分岐と関係演算子
+# # 条件分岐と関係演算子
 
-if tall > 3:
-    print('ooii')
-else:
-  print('sukunai')
+# if tall > 3:
+#     print('ooii')
+# else:
+#   print('sukunai')
+
+# 関数 引数arg
+def test_python(arg):
+    test_status = arg
+
+    if(test_status < 10):
+        return 'sukunai'
+    else:
+        return 'ooi'
+
+
+print(test_python(9))
